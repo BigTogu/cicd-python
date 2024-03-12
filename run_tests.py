aa49791bb40319585f903f6638127d374b1a8f71
bigTogu <silvia.kenaan@gmail.com>
2024-03-12 20:07:24 +0100

    feat: Add test runner script

---
+++ run_tests.py
@@ -0,0 +1,8 @@
+import unittest
+from test_suma import TestSumar
+from test_resta import TestRestar
+from test_multiplicacion import TestMultiplicar
+from test_division import TestDividir
+
+if __name__ == '__main__':
+    unittest.main()
