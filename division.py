38b42128394b3f54599ddab64b1fa3541cb00b89
bigTogu <silvia.kenaan@gmail.com>
2024-03-12 20:03:13 +0100

    feat: Add division.py and test_division.py

---
+++ division.py
@@ -0,0 +1,8 @@
+def dividir(a, b):
+  if b == 0:
+    raise ValueError("No se puede dividir por cero")
+  return a / b
+
+
+if __name__ == "__main__":
+  print(dividir(5, 3))
---
+++ test_division.py
@@ -0,0 +1,15 @@
+import unittest
+from division import dividir
+
+
+
+class TestDivision(unittest.TestCase):
+  def test_dividir(self):
+    self.assertEqual(dividir(6, 3), 2)
+    self.assertEqual(dividir(0, 12), 0)
+    self.assertEqual(dividir(-1, -1), 1)
+    self.assertRaises(ValueError, dividir, 1, 0)
+
+
+if __name__ == '__main__':
+  unittest.main()
