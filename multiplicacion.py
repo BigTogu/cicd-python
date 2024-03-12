dcf8f8c46c5056fb94070fc9975f474f43e14902
bigTogu <silvia.kenaan@gmail.com>
2024-03-12 19:55:53 +0100

    feat: Add multiplication function and test cases

---
+++ multiplicacion.py
@@ -0,0 +1,6 @@
+def multiplicar(a, b):
+  return a * b
+
+
+if __name__ == "__main__":
+  print(multiplicar(5, 3))
---
+++ test_multiplicacion.py
@@ -0,0 +1,14 @@
+import unittest
+from multiplicacion import multiplicar
+
+
+
+class TestMultiplicar(unittest.TestCase):
+  def test_multiplicar(self):
+    self.assertEqual(multiplicar(3, 2), 6)
+    self.assertEqual(multiplicar(-1, 1), -1)
+    self.assertEqual(multiplicar(-1, -1), 1)
+
+
+if __name__ == '__main__':
+  unittest.main()
